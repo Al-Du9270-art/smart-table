4ee57efcbc0cc040c98c9dae216f0bcf9b58b311
Aleksander Dushkin <al.du9270@gmail.com>
2026-05-01 21:16:07 +0500

    first push

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -4,11 +4,22 @@ import {createComparison, defaultRules} from "../lib/compare.js";
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
-
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+      .forEach((elementName) => {                        // Перебираем по именам
+        elements[elementName].append(                    // в каждый элемент добавляем опции
+            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                      .map(name => {                        // используйте name как значение и текстовое содержимое
+                        let opt = document.createElement("option");
+                        opt.setAttribute('value', name);
+                        opt.textContent = name;
+                        return opt                      // @todo: создать и вернуть тег опции
+                      })
+        )
+     })
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
-
+        const compare = createComparison(defaultRules);
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
